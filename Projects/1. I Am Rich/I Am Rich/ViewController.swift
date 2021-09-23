d90ba10057b18a01eb94f2c0293c1b1731a9f135
usamaeltmsah <usamaeltmsah@yahoo.com>
2021-09-23 16:16:34 +0200

    I am rich project which is a basic ui project

---
+++ Projects/1. I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Usama Fouad on 26/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
