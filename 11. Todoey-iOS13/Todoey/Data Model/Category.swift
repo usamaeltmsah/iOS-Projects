d5d2375f8333768fbe29e6caf38643e5a95444e8
Usama Fouad <usamaeltmsah@gmail.com>
2021-10-15 02:45:53 +0200

    Add a Category and Item closes to conforms to our Real database entities

---
+++ 11. Todoey-iOS13/Todoey/Data Model/Category.swift
@@ -0,0 +1,9 @@
+//
+//  Category.swift
+//  Todoey
+//
+//  Created by Usama Fouad on 15/10/2021.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ 11. Todoey-iOS13/Todoey/Data Model/Item.swift
@@ -0,0 +1,9 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Usama Fouad on 15/10/2021.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
