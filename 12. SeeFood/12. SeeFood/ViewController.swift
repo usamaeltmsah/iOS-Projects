b9bff10131e1b54edd6caad83a6fe704621ca29e
Usama Fouad <usamaeltmsah@gmail.com>
2021-10-24 00:11:16 +0200

    Use CoreML to detect the user's image if it a hotdog or not

--- 12. SeeFood/12. SeeFood/ViewController.swift
+++ 12. SeeFood/12. SeeFood/ViewController.swift
@@ -26,9 +26,48 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let userPickedImage = info[.originalImage] as? UIImage {
             imageView.image = userPickedImage
+            
+            guard let ciImage = CIImage(image: userPickedImage) else {
+                fatalError("Couldn't convert UIImage into CIImage")
+            }
+            
+            detect(image: ciImage)
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(image: CIImage) {
+        let config = MLModelConfiguration()
+        guard let model = try? VNCoreMLModel(for: MobileNetV2(configuration: config).model) else {
+            fatalError("Loading CoreML model failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { request, error in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image")
+            }
+//            print(results)
+            
+            if let firstResult = results.first {
+                if firstResult.identifier.contains("hotdog") {
+                    self.navigationController?.navigationBar.barTintColor = .green
+                    self.title = "Hotdog!"
+                } else {
+                    self.navigationController?.navigationBar.barTintColor = .red
+                    self.title = "Not a hotdog!"
+                }
+            }
+        }
+        
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
     }
 
     @IBAction func cameraTapped(_ sender: Any) {
