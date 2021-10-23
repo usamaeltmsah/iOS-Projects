e368ac8509d6c58c55cd203f086ac631ca324c33
Usama Fouad <usamaeltmsah@gmail.com>
2021-10-23 22:55:23 +0200

    Add the MobileNetV2 Model file

---
+++ 12. SeeFood/12. SeeFood/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  12. SeeFood
+//
+//  Created by Usama Fouad on 23/10/2021.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
