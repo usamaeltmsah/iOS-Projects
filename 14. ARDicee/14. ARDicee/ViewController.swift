3d4ddcf0e3cbffd1c9797c4bc3ea6fd33095ef1a
Usama Fouad <usamaeltmsah@gmail.com>
2021-11-04 23:22:59 +0200

    let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
            let material = SCNMaterial()
            material.diffuse.contents = UIColor.red
            cube.materials = [material]
    
            let node = SCNNode()
            node.position = SCNVector3(0, 0.1, -0.5)

--- 14. ARDicee/14. ARDicee/ViewController.swift
+++ 14. ARDicee/14. ARDicee/ViewController.swift
@@ -19,16 +19,26 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
+        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
         
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.red
+        cube.materials = [material]
+        
+        let node = SCNNode()
+        node.position = SCNVector3(0, 0.1, -0.5)
+        node.geometry = cube
+        sceneView.scene.rootNode.addChildNode(node)
+        sceneView.autoenablesDefaultLighting = true
         
         // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-
-        // Set the scene to the view
-        sceneView.scene = scene
+//        sceneView.showsStatistics = true
+//
+//        // Create a new scene
+//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+//
+//        // Set the scene to the view
+//        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
