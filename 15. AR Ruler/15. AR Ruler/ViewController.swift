e27aa65edc101ac6c5b678fa2ee2d1b2d61ffaae
Usama Fouad <usamaeltmsah@gmail.com>
2021-11-08 21:36:24 +0200

    Resetting the Measurements

--- 15. AR Ruler/15. AR Ruler/ViewController.swift
+++ 15. AR Ruler/15. AR Ruler/ViewController.swift
@@ -12,6 +12,7 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     var dotNodes = [SCNNode]()
+    var textNode = SCNNode()
     
     @IBOutlet var sceneView: ARSCNView!
     
@@ -42,6 +43,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if dotNodes.count >= 2 {
+            for dot in dotNodes {
+                dot.removeFromParentNode()
+            }
+            dotNodes = [SCNNode]()
+        }
+        
         if let touch = touches.first {
             let touchLocation = touch.location(in: sceneView)
             
@@ -86,13 +94,15 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
     
     func updateText(text: String, at position: SCNVector3) {
+        textNode.removeFromParentNode()
+        
         let textGeometry = SCNText(string: text, extrusionDepth: 1.0)
         
         textGeometry.firstMaterial?.diffuse.contents = UIColor.red
         
-        let textNode = SCNNode(geometry: textGeometry)
+        textNode = SCNNode(geometry: textGeometry)
         
-        textNode.position = SCNVector3(position.x, position.y, position.z)
+        textNode.position = SCNVector3(position.x, position.y + 0.01, position.z)
         
         textNode.scale = SCNVector3(0.01, 0.01, 0.01)
         
