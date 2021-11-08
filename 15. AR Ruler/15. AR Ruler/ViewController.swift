1b0101196930998e9eb29aa14c6fe6dcc7e70c41
Usama Fouad <usamaeltmsah@gmail.com>
2021-11-08 21:04:33 +0200

    Initialize the AR ruler project, which is an app to measure the user selected range

---
+++ 15. AR Ruler/15. AR Ruler/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  15. AR Ruler
+//
+//  Created by Usama Fouad on 08/11/2021.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+        
+        sceneView.debugOptions = [.showFeaturePoints]
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+}
